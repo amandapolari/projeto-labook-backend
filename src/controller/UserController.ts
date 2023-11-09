68f583d698efdc91cca392284c9b373c9e49a7e1
Amanda <amandaoliveirapolari@gmail.com>
2023-11-09 11:09:37 -0300

    [refactor] applying routing middleware and custom errors

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { UserBusiness } from '../business/UserBusiness';
+import { BaseError } from '../errors/BaseError';
 
 export class UserController {
     // GET
@@ -15,15 +16,10 @@ export class UserController {
             res.status(200).send(output);
         } catch (error) {
             console.log(error);
-
-            if (req.statusCode === 200) {
-                res.status(500);
-            }
-
-            if (error instanceof Error) {
-                res.send(error.message);
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message);
             } else {
-                res.send('Erro inesperado');
+                res.status(500).send('Erro inesperado');
             }
         }
     };
@@ -45,15 +41,10 @@ export class UserController {
             res.status(201).send(output);
         } catch (error) {
             console.log(error);
-
-            if (req.statusCode === 200) {
-                res.status(500);
-            }
-
-            if (error instanceof Error) {
-                res.send(error.message);
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message);
             } else {
-                res.send('Erro inesperado');
+                res.status(500).send('Erro inesperado');
             }
         }
     };
@@ -77,15 +68,10 @@ export class UserController {
             res.status(200).send(output);
         } catch (error) {
             console.log(error);
-
-            if (req.statusCode === 200) {
-                res.status(500);
-            }
-
-            if (error instanceof Error) {
-                res.send(error.message);
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message);
             } else {
-                res.send('Erro inesperado');
+                res.status(500).send('Erro inesperado');
             }
         }
     };
@@ -103,15 +89,10 @@ export class UserController {
             res.status(200).send(output);
         } catch (error) {
             console.log(error);
-
-            if (req.statusCode === 200) {
-                res.status(500);
-            }
-
-            if (error instanceof Error) {
-                res.send(error.message);
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message);
             } else {
-                res.send('Erro inesperado');
+                res.status(500).send('Erro inesperado');
             }
         }
     };
